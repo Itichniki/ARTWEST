75196ab7b58d847e217b1ff4d46117a7fc008053
Podik <22stalkerpro22@gmail.com>
2025-03-12 21:39:52 +0200

    Adding user controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -15,21 +15,21 @@ const generateJwt = (id, email, role) => {
 
 class UserController {
 
-    async register(req, res) {
+    async register(req, res, next) {
 
         const { email, password, role } = req.body;
-        console.log(req.body);
+
         if(!email) {
-            return ApiError.badRequest("Email should be provided!");
+            return next(ApiError.badRequest("Email should be provided!"));
         }
 
         if(!password) {
-            return ApiError.badRequest("Password should be provided!");
+            return next(ApiError.badRequest("Password should be provided!"));
         }
 
-        const candidate = await User.findOne({email: email});
+        const candidate = await User.findOne({where: {email}});
         if (candidate) {
-            return ApiError.internal("User already exists");
+            return next(ApiError.badRequest("User already exists"));
         }
 
         const hashedPassword = await bcrypt.hash(password, 10);
@@ -39,22 +39,23 @@ class UserController {
         return res.json({token});
     }
 
-    async login(req, res) {
+    async login(req, res, next) {
 
         const { email, password } = req.body;
         if(!email || !password) {
-            return ApiError.badRequest("Email or password is wrong");
+            return next(ApiError.badRequest("Email or password is wrong"));
         }
 
-        const user = await User.findOne({email: email});
+        const user = await User.findOne({where: {email}});
 
         if(!user) {
-            return ApiError.badRequest("User does not exist!");
+            return next(ApiError.badRequest("User does not exist!"));
         }
 
         const checkPassword = await bcrypt.compare(password, user.password);
+
         if (!checkPassword) {
-            return ApiError.badRequest("Password is wrong!");
+            return next(ApiError.badRequest("Password is wrong!"));
         }
 
         const token = generateJwt(user.id, user.email, user.role);
@@ -62,7 +63,12 @@ class UserController {
     }
 
     async check(req, res) {
-
+        try {
+            const token = generateJwt({id: req.user.id, email: req.user.email, role: req.user.role});
+            return res.json({token});
+        } catch (e) {
+            console.log(e);
+        }
     }
 
 }
