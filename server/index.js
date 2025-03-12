c2719c4bc4c7a2152cbeec871b960941bf5e3d36
Podik <22stalkerpro22@gmail.com>
2025-03-12 21:40:35 +0200

    Changing direction of middlewares

--- server/index.js
+++ server/index.js
@@ -7,15 +7,15 @@ import errorHandler from "./middleware/errorHandlingMiddleware.js";
 const app = express();
 
 app.use(express.json());
-app.use(errorHandler());
 app.use(cors());
 app.use("/api", router);
+app.use(errorHandler);
 
 const start = async () => {
     await sequelize.authenticate();
     await sequelize.sync();
     app.listen(process.env.PORT, () => {
-        console.log("Server started on port 5000");
+        console.log(`Server started on port ${process.env.PORT}`);
     });
 }
 
